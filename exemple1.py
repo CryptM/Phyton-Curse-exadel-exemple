ee33686d931352eeb7f083ec8477060ff2ecc515
CryptM <cryptm@gmail.com>
2016-06-27 12:44:27 +0000

    Task 1

---
+++ exemple1.py
@@ -0,0 +1,9 @@
+x=input ('''1. Soup and salad\n2. Pasta with meat sauce\n3. Chef's special\nWhich number would you like to order?''')
+if x=='1':
+  print('Soup and salad coming right up!')
+elif x=='2':
+  print('One Pasta with meat sauce coming right up!')
+elif x=='3':
+  print("Chef's special coming right up!")
+else:
+  print('Sorry, that is not a valid choice.')
